1c221ad6c1a017ae7013b7c773ac74e453a0f6aa
ch il <792534005@qq.com>
2018-06-14 21:10:37 +0800

    fix value

--- src/App.js
+++ src/App.js
@@ -22,12 +22,14 @@ class App extends Component {
         </li>
       )
     })
-    console.log(todos)
+
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
+          <TodoInput content={this.state.newTodo}
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol>
           {todos}
@@ -35,6 +37,14 @@ class App extends Component {
       </div>
     )
   }
+  changeTitle(event) {
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+
+
   addTodo(event) {
     this.state.todoList.push({
       id: idMaker(),
